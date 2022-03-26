2cc71acc272a46fe5b053d9f4461c4d1dd78d042
Milot <55019464+MilotH2@users.noreply.github.com>
2022-03-26 15:41:41 +0100

    initialized chessboard card
    initialized chessboard card, on ts, html and scss for chessboard card and colored box, showing only one dark color on column

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ChessBoard } from '../models/chessBoard';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  // this is the chess board it self, with 8x8 squares
+  chessBoard: any;
+
+  // show board after initialization
+  showChessBoard: boolean = false;
 
   constructor() {}
 
+  ngOnInit() {
+    this.initializeChessBoard();
+  }
+
+  // Initializes the chess board
+  initializeChessBoard() {
+    this.chessBoard = ChessBoard.emptyBoard;
+    this.showChessBoard = true;
+  }
+
+  // empties the board
+  clearBoard() {
+    this.chessBoard = ChessBoard.emptyBoard;
+  }
+
+  // Colors the Chess Board on condition, to show white and black color of the box
+  addBoxColor(rowIndex, colIndex) {
+    return (rowIndex + colIndex) % 2 === 1;
+  }
 }
