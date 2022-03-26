4ede39e0261bee1f5a89e277702b688ee3ae598a
Milot <55019464+MilotH2@users.noreply.github.com>
2022-03-26 16:31:12 +0100

    added select box
    added style for highlighted box, free box and selected new color for creating new pawn
    
    added availablesteps of the selected pawn array
    added a restrict movement check condition before selecting a pawn or moving a pawn to other box or selecting a box to create a new pawn
    
    added select paw or move to other box function
    
    added select pawn fill data function

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ChessBoard } from '../models/chessBoard';
+import { CardionalDirections } from '../models/chessEnums';
 import { Pawn } from '../models/pawn';
 import { PawnService } from '../services/pawn.service';
 
@@ -57,6 +58,7 @@ export class HomePage implements OnInit {
     this.colIndex = -1;
     this.rowIndex = -1;
     this.isCreatingNewPawn = false;
+    this.availableStepsOfSelectedPawn = [];
     this.selectedPawn = null;
   }
 
@@ -69,6 +71,136 @@ export class HomePage implements OnInit {
       this.selectedDirection
     );
     console.log(newPawn);
+
+    // this.checkConditionOfThePawnAndFillAvailablePawnSteps(
+    //   this.selectedPawn,
+    //   rowIndex,
+    //   colIndex
+    // );
+  }
+
+  // if this function returns true, it means that the pawn can not move to other box, if false the pawn can move to other box square
+  restrictMovement(chessBoardColumn: Pawn | null, rowIndex, colIndex) {
+    console.log({
+      chessBoardColumn,
+      rowIndex,
+      colIndex,
+      rowIndexOnTS: this.rowIndex,
+      colIndexOnTS: this.colIndex,
+      pawn: this.selectedPawn,
+    });
+
+    // check if there is a selected pawn and clear pawn if user selects on an empty box
+    if (this.selectedPawn) {
+      if (this.selectedPawn.facing == 0 || this.selectedPawn.facing == 2) {
+        if (this.rowIndex == rowIndex || this.colIndex != colIndex) {
+          this.clearSelectedPawnAndIndexes();
+        }
+      } else if (this.selectedPawn.facing == 1 || this.selectedPawn.facing == 3)
+        if (this.rowIndex != rowIndex || this.colIndex == colIndex) {
+          this.clearSelectedPawnAndIndexes();
+        }
+    }
+
+    // clear data if there is no pawn selected and no pawn on the selected box
+    if (!chessBoardColumn && !this.selectedPawn) {
+      this.colIndex = colIndex;
+      this.rowIndex = rowIndex;
+      this.availableStepsOfSelectedPawn = [];
+      this.isCreatingNewPawn = true;
+      this.selectedColor = 'WHITE';
+      this.selectedDirection = 0;
+      this.selectedPawn = null;
+      return true;
+    }
+
+    // clear data if there is plan to create a new PAWN
+    if (this.isCreatingNewPawn) {
+      this.clearSelectedPawnAndIndexes();
+    }
+
+    // exclude all steps except possible paths
+    if (this.colIndex !== -1 && this.rowIndex !== -1) {
+      // return the opposite of showFreeBoxAndCheckAvailablePawnStepsList
+      let excludedOrNo = this.showFreeBoxAndCheckAvailablePawnStepsList(
+        rowIndex,
+        colIndex
+      );
+      return !excludedOrNo;
+    }
+    // select pawn
+    return false;
+  }
+
+  // on select of PAWN or EMPTY AVAILABLE BOX to move PAWN to other BOX
+  selectPawnOrMoveToOtherBox(
+    chessBoardColumn: Pawn | null,
+    rowIndex,
+    colIndex
+  ) {
+    console.table(this.chessBoard);
+    // empty all other paths
+    this.availableStepsOfSelectedPawn = [];
+
+    // if there is a selected pawn
+    if (this.colIndex != -1 && this.rowIndex != -1) {
+      if (chessBoardColumn && this.selectedPawn) {
+        // select other pawn if exists
+        this.selectPawnFillData(chessBoardColumn, rowIndex, colIndex);
+      } else {
+        // update selectedPawn indexes with upcoming index
+        this.selectedPawn.rowIndex = rowIndex;
+        this.selectedPawn.colIndex = colIndex;
+
+        // empty current occupied box with selected PAWN
+        this.chessBoard[this.rowIndex][this.colIndex] = null;
+
+        // relocate selected pawn to other selected available box
+        this.chessBoard[rowIndex][colIndex] = this.selectedPawn;
+
+        // update new selected box indexes
+        this.colIndex = colIndex;
+        this.rowIndex = rowIndex;
+        this.checkConditionOfThePawnAndFillAvailablePawnSteps(
+          this.selectedPawn,
+          rowIndex,
+          colIndex
+        );
+      }
+    } else {
+      // select pawn for the first time
+      this.selectPawnFillData(chessBoardColumn, rowIndex, colIndex);
+    }
+  }
+
+  // this function fills data of the pawn when it is selected a new one or other pawn
+  selectPawnFillData(chessBoardColumn, rowIndex, colIndex) {
+    this.selectedPawn = chessBoardColumn;
+    this.selectedPawn.rowIndex = rowIndex;
+    this.selectedPawn.colIndex = colIndex;
+    this.colIndex = colIndex;
+    this.rowIndex = rowIndex;
+    this.checkConditionOfThePawnAndFillAvailablePawnSteps(
+      chessBoardColumn,
+      rowIndex,
+      colIndex
+    );
+  }
+
+  // chech condition of the pawn based on the facing direction
+  checkConditionOfThePawnAndFillAvailablePawnSteps(
+    chessBoardColumn: Pawn,
+    rowIndex,
+    colIndex
+  ) {
+    if (chessBoardColumn.facing == CardionalDirections.WEST) {
+    }
+    if (chessBoardColumn.facing == CardionalDirections.SOUTH) {
+    }
+    if (chessBoardColumn.facing == CardionalDirections.EAST) {
+    }
+    if (chessBoardColumn.facing == CardionalDirections.NORTH) {
+    }
   }
 
   // Colors the Chess Board on condition, to show white and black color of the box
